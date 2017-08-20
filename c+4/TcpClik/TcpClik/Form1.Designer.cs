1e99475f835625ff23055000df124df4946e42d9
heishou <317027431@qq.com>
2017-08-20 23:40:08 +0800

    jl

---
+++ c+4/TcpClik/TcpClik/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TcpClik
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.StartClik = new System.Windows.Forms.Button();
+			this.StopClik = new System.Windows.Forms.Button();
+			this.textclik = new System.Windows.Forms.TextBox();
+			this.Sendclik = new System.Windows.Forms.Button();
+			this.tblog = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// StartClik
+			// 
+			this.StartClik.Location = new System.Drawing.Point(12, 12);
+			this.StartClik.Name = "StartClik";
+			this.StartClik.Size = new System.Drawing.Size(75, 23);
+			this.StartClik.TabIndex = 0;
+			this.StartClik.Text = "启动客户端";
+			this.StartClik.UseVisualStyleBackColor = true;
+			// 
+			// StopClik
+			// 
+			this.StopClik.Location = new System.Drawing.Point(123, 11);
+			this.StopClik.Name = "StopClik";
+			this.StopClik.Size = new System.Drawing.Size(75, 23);
+			this.StopClik.TabIndex = 1;
+			this.StopClik.Text = "关闭客户端";
+			this.StopClik.UseVisualStyleBackColor = true;
+			// 
+			// textclik
+			// 
+			this.textclik.Location = new System.Drawing.Point(3, 51);
+			this.textclik.Multiline = true;
+			this.textclik.Name = "textclik";
+			this.textclik.Size = new System.Drawing.Size(213, 305);
+			this.textclik.TabIndex = 2;
+			// 
+			// Sendclik
+			// 
+			this.Sendclik.Location = new System.Drawing.Point(222, 51);
+			this.Sendclik.Name = "Sendclik";
+			this.Sendclik.Size = new System.Drawing.Size(75, 23);
+			this.Sendclik.TabIndex = 3;
+			this.Sendclik.Text = "发送";
+			this.Sendclik.UseVisualStyleBackColor = true;
+			// 
+			// tblog
+			// 
+			this.tblog.Location = new System.Drawing.Point(353, 51);
+			this.tblog.Multiline = true;
+			this.tblog.Name = "tblog";
+			this.tblog.Size = new System.Drawing.Size(209, 298);
+			this.tblog.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(574, 361);
+			this.Controls.Add(this.tblog);
+			this.Controls.Add(this.Sendclik);
+			this.Controls.Add(this.textclik);
+			this.Controls.Add(this.StopClik);
+			this.Controls.Add(this.StartClik);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button StartClik;
+		private System.Windows.Forms.Button StopClik;
+		private System.Windows.Forms.TextBox textclik;
+		private System.Windows.Forms.Button Sendclik;
+		private System.Windows.Forms.TextBox tblog;
+	}
+}
+
---
+++ c+4/TcpClik/TcpClik/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TcpClik
+{
+	public partial class Form1 : Form
+	{
+		string hostip;
+		TcpClient client;
+		NetworkStream senStren;
+		BinaryWriter bw;
+		String st;
+		public Form1()
+		{
+			InitializeComponent();
+			StartClik.Click += StartClik_Click;
+			StopClik.Click += StopClik_Click;
+			Sendclik.Click += Sendclik_Click;
+		}
+
+		private void Sendclik_Click(object sender, EventArgs e)
+		{
+
+			
+			st = "CS123";
+		    st = textclik.Text;
+		//	logs("发送到服务端" + st);
+		//	senStren = client.GetStream();
+		//	if (senStren.CanWrite)
+		//	{
+		//		Byte[] sendBytes = Encoding.Default.GetBytes(st);
+		//		senStren.Write(sendBytes, 0, sendBytes.Length);
+		//		senStren.Flush();
+		//		senStren.Close();//关闭网络流  
+		//						   //client.Close();//关闭客户端  
+
+		//	}
+		//	else
+		//	{
+		//		MessageBox.Show("无法写入数据流");
+
+		//		senStren.Close();
+		//		client.Close();
+
+		//		return;
+
+
+
+
+		//	}
+		}
+
+		private void StopClik_Click(object sender, EventArgs e)
+		{
+			senStren.Close();
+		}
+
+		private void StartClik_Click(object sender, EventArgs e)
+		{
+			string strHostName = Dns.GetHostName(); //得到本机的主机名
+			IPHostEntry ipEntry = Dns.GetHostByName(strHostName); //取得本机IP
+			hostip = ipEntry.AddressList[0].ToString();
+			client = new TcpClient();
+			client.Connect(hostip, 5252);
+			if (client!=null)
+			{
+				logs("链接成功");
+				NetworkStream networkStream = client.GetStream();
+				BinaryReader br = new BinaryReader(networkStream);
+				 bw = new BinaryWriter(networkStream);
+				st = "CS123";
+				st = textclik.Text;
+				bw.Write(st);  //向服务器发送字符串
+				while (true)
+				{
+					try
+					{
+						string brString = br.ReadString();     //接收服务器发送的数据  
+						if (brString != null)
+						{
+							Console.WriteLine("接收到服务器发送的数据{0}", brString);
+						}
+					}
+					catch
+					{
+						break;        //接收过程中如果出现异常，将推出循环  
+					}
+				}
+			}
+			Console.WriteLine("连接服务器失败");
+		
+		}
+		public void logs(string log)
+		{
+			tblog.AppendText($"[{DateTime.Now.ToString("HH:mm:ss")}] {log}\r\n");
+		}
+	}
+}
