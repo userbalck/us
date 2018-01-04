3d3b023763b09a9b1d072d2fd35e5a10872ebdad
sumin <sumin@streamax.com>
2018-01-04 16:11:57 +0800

    手动解决冲突代码

--- selenium/Appium/src/com/Seleium/www/Assist.java
+++ selenium/Appium/src/com/Seleium/www/Assist.java
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-package com.Seleium.www;
 
+package com.Seleium.www;
 public class Assist {
 	
 
@@ -26,32 +25,4 @@ public class Assist {
 	 }
 	 
  }
-=======
-package com.Seleium.www;
 
-public class Assist {
-	
-
-	 public static void ty(int tiem) throws Exception {
-		  try {
-			  System.out.println("µÈ´ýÃë£º"+tiem);
-			  int t=tiem*1000;
-			   Thread.sleep(t);
-			   
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
-	 }
-	 public static void pn(String s){
-		 
-		 System.out.println(s);
-		 
-	 }  
-	 public static void pt(String s){
-		 
-		 System.out.print(s);
-		 
-	 }
-	 
- }
->>>>>>> 1cce76996a288d93e2decf71290fc3556ef23947
